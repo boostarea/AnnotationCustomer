04d9494a0006b5cdac2eae6954abe513e54cf1e6
rong.chen <18649808198@163.com>
2017-08-26 21:50:42 +0800

    modify output to Hello Master Chen

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Master";
+		return "Hello Master Chen";
 	}
 
 	public static void main(String[] args) {
