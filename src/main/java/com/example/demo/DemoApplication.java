f105d62555fbdb291954919f171a7f19b900eb86
rong.chen <18649808198@163.com>
2017-08-26 22:04:26 +0800

    Hello Master Chen ！

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Master Chen";
+		return "Hello Master Chen ！";
 	}
 
 	public static void main(String[] args) {
